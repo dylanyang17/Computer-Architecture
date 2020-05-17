9b9d142c747e152e4dc3949d674769467628db2c
yangyr17 <315629555@qq.com>
2020-05-18 03:41:09 +0800

    fix(test): 通过测试

--- PA2/src/test.cpp
+++ PA2/src/test.cpp
@@ -11,11 +11,11 @@ void test() {
     printf("测试中...\n");
     vector<Instruction> instructions = Instruction::readFile("TestCase/Fact.nel");
     assert(instructions.size() == 10);
-    assert(instructions[2] == Instruction(Instruction::Type::LOAD, 6, 0x1E));
+    assert(instructions[2] == Instruction(Instruction::Type::LOAD, 2, 0x1E));
     assert(instructions[4] == Instruction(Instruction::Type::MUL, 0, 0, 1));
     assert(instructions[5] == Instruction(Instruction::Type::SUB, 3, 2, 1));
     assert(instructions[6] == Instruction(Instruction::Type::ADD, 1, 4, 1));
     assert(instructions[7] == Instruction(Instruction::Type::JUMP, 0x0, 3, 0x2));
-    assert(instructions[7] == Instruction(Instruction::Type::JUMP, 0x0, 31, 0xFFFFFFFC));
+    assert(instructions[8] == Instruction(Instruction::Type::JUMP, 0x0, 31, 0xFFFFFFFC));
     printf("通过测试, 开始运行...\n");
 }
